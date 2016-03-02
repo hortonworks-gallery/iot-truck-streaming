02ae88abf6042f50da3a1dc12700fe8738454d86
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2016-03-02 13:10:24 -0800

    Update install_activemq.sh
    Updated link to download ActiveMQ 5.9 (previous link to SNAPSHOT seems to be broken)

--- setup/bin/install_activemq.sh
+++ setup/bin/install_activemq.sh
@@ -2,7 +2,9 @@
 
 mkdir /opt/activemq
 cd /opt/activemq
-wget https://repository.apache.org/content/repositories/snapshots/org/apache/activemq/apache-activemq/5.9-SNAPSHOT/apache-activemq-5.9-20131010.203434-114-bin.tar.gz
+#wget https://repository.apache.org/content/repositories/snapshots/org/apache/activemq/apache-activemq/5.9-SNAPSHOT/apache-activemq-5.9-20131010.203434-114-bin.tar.gz
+wget http://archive.apache.org/dist/activemq/apache-activemq/5.9.0/apache-activemq-5.9.0-bin.tar.gz
 tar xvzf apache-activemq-*.tar.gz
-ln -s apache-activemq-5.9-SNAPSHOT latest
+#ln -s apache-activemq-5.9-SNAPSHOT latest
+ln -s apache-activemq-* latest
 cd latest
