e5ef9865aba0055bc11924bc111753ae5129656d
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2016-03-02 17:18:37 -0800

    Update install_activemq.sh
    ln requires full dirname

--- setup/bin/install_activemq.sh
+++ setup/bin/install_activemq.sh
@@ -6,5 +6,5 @@ cd /opt/activemq
 wget http://archive.apache.org/dist/activemq/apache-activemq/5.9.0/apache-activemq-5.9.0-bin.tar.gz
 tar xvzf apache-activemq-*.tar.gz
 #ln -s apache-activemq-5.9-SNAPSHOT latest
-ln -s apache-activemq-* latest
+ln -s apache-activemq-5.9.0 latest
 cd latest
